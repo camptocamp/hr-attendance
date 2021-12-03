3be203dd41fd2b708c08270dcaaf21ce751a1765
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-12-03 10:20:57 +0100

    14.0 [ADD] hr_attendance_hours_report
    This module provides a new report in hr_attendance to display the worked
    time of employees split in the following categories:
    
    * type of day (weekday, sunday, holiday)
    * daytime / nighttime
    
    The information of a day being a holiday comes from the configured
    public holidays from the module hr_holidays_public.
    
    This is meant as a helper for people interacting with external payroll systems.

---
+++ hr_attendance_hours_report/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'HR Attendance hours report',
+    'description': "Report day/night/sunday work hours for employees based on attendances",
+    'version': '14.0.1.0.0',
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr-attendance",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_attendance',
+        'hr_holidays_public',
+    ],
+    'data': [
+        "views/res_config_settings.xml",
+        "views/hr_attendance.xml",
+    ],
+    'installable': True,
+}
---
+++ hr_attendance_hours_report/models/res_company.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    hr_night_work_hour_start = fields.Float("Begining of night work time", default=22, help="expressed in company timezone")
+    hr_night_work_hour_end = fields.Float("End of night work time", default=6, help="expressed in company timezone")

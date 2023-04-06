60995dcac641313e03c116d33b95bde6c627d96e
Dũng (Trần Đình) <dungtd@trobz.com>
2023-04-06 18:26:14 +0700

    [MIG] hr_attendance_report_theoretical_time: Migration to 16.0

--- hr_attendance_report_theoretical_time/__manifest__.py
+++ hr_attendance_report_theoretical_time/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Theoretical vs Attended Time Analysis",
-    "version": "15.0.1.1.1",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["hr_attendance", "hr_holidays_public"],
+    "depends": ["hr_attendance", "hr_holidays_public", "test_resource"],
     "data": [
         "security/ir.model.access.csv",
         "security/hr_attendance_report_theoretical_time_security.xml",
